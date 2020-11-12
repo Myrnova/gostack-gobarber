b759f0440e928ec7b5ee99a5c8b22416a515aaf0
Débora Cavallari <debora_cavallari@hotmail.com>
2020-11-12 15:50:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<h2 align="center">
+  <img src="https://img.shields.io/badge/BOOTCAMP%20GO%20STACK-Em%20Andamento-yellow?style=for-the-badge" alt="Bootcamp GoStack" />
+</h2>
+
+## 📌 Index
+
+- [Sobre](#-sobre-o-projeto)
+
+
+## ❔ Sobre
+
+Desafios proposta sobre a aula de conceitos node.js
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({
+    provider_id,
+    date,
+  }: RequestDTO): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
+
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already scheduled');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
