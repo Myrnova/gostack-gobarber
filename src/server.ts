2611acf02cbe02dc12a948a04d4447610e52e25c
Myrnova <debora_cavallari@hotmail.com>
2021-01-04 15:02:35 -0300

    Front-end

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import 'reflect-metadata';
 import routes from './routes';
 import './database';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
